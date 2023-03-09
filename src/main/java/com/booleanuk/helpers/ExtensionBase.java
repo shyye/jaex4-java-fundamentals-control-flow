8d3cfed1bff035f6cbffaae4ab312c2cdeccb131
Dave Ames <david.john.ames@gmail.com>
2023-03-09 17:31:27 +0000

    Added an ExtensionBase class that the Extension class inherits from to fix the problems caused by missing methods.

---
+++ src/main/java/com/booleanuk/helpers/ExtensionBase.java
@@ -0,0 +1,15 @@
+package com.booleanuk.helpers;
+
+public class ExtensionBase {
+    public String timerStatus(int remaining) {
+        return "Not implemented yet!";
+    }
+
+    public int estimatePrepTime(String[] ingredients, int times) {
+        return -1;
+    }
+
+    public int calculateGramsOfSugar(String[] ingredients, int layers) {
+        return -1;
+    }
+}
