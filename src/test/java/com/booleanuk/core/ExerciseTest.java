a8ec08bc9208e206340ed81a93b342a86d4bf706
Dave Ames <david.john.ames@gmail.com>
2023-03-16 10:13:57 +0000

    Revert "Added method ordering annotations to make it so that the tests run in the correct order and display failures too."
    This reverts commit 1729be140bfaa207c1fe75f2385af92452a5afd0.

--- src/test/java/com/booleanuk/core/ExerciseTest.java
+++ src/test/java/com/booleanuk/core/ExerciseTest.java
@@ -2,11 +2,7 @@ package com.booleanuk.core;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ExerciseTest {
     Exercise exercise;
 
@@ -15,37 +11,31 @@ class ExerciseTest {
     }
 
     @Test
-    @Order(1)
     public void answerOne() {
         Assertions.assertEquals("Good day!", this.exercise.one());
     }
 
     @Test
-    @Order(2)
     public void answerTwo() {
         Assertions.assertEquals("Good morning!", this.exercise.two());
     }
 
     @Test
-    @Order(3)
     public void answerThree() {
         Assertions.assertEquals("Good morning!", this.exercise.three());
     }
 
     @Test
-    @Order(4)
     public void answerFour() {
         Assertions.assertEquals("Good morning!", this.exercise.four());
     }
 
     @Test
-    @Order(5)
     public void answerFive() {
         Assertions.assertEquals("Good day!", this.exercise.five());
     }
 
     @Test
-    @Order(6)
     public void answerSix() {
         Assertions.assertEquals("Correct!", this.exercise.six(8));
         Assertions.assertEquals("Wrong!", this.exercise.six(6));
@@ -53,14 +43,12 @@ class ExerciseTest {
     }
 
     @Test
-    @Order(7)
     public void answerSeven() {
         Assertions.assertEquals("Correct!", this.exercise.seven(false));
         Assertions.assertEquals("Wrong!", this.exercise.seven(true));
     }
 
     @Test
-    @Order(8)
     public void answerEight() {
         Assertions.assertEquals("Correct!", this.exercise.eight(8, 8));
         Assertions.assertEquals("Correct!", this.exercise.eight(10, 8));
@@ -68,21 +56,18 @@ class ExerciseTest {
     }
 
     @Test
-    @Order(9)
     public void answerNine() {
         Assertions.assertTrue(this.exercise.nine(new int[]{3, 6, 7}));
         Assertions.assertFalse(this.exercise.nine(new int[0]));
     }
 
     @Test
-    @Order(10)
     public void answerTen() {
         Assertions.assertTrue(this.exercise.ten("I need to buy milk and coffee"));
         Assertions.assertFalse(this.exercise.ten("I need to buy tea and coffee"));
     }
 
     @Test
-    @Order(11)
     public void answerEleven() {
         Assertions.assertEquals(3, this.exercise.eleven("I need to buy milk"));
         Assertions.assertEquals(6, this.exercise.eleven("I need to buy coffee"));
@@ -91,7 +76,6 @@ class ExerciseTest {
     }
 
     @Test
-    @Order(12)
     public void answerTwelve() {
         Assertions.assertTrue(this.exercise.twelve(5, 5, 5));
         Assertions.assertTrue(this.exercise.twelve(5, 3, 7));
@@ -102,7 +86,6 @@ class ExerciseTest {
     }
 
     @Test
-    @Order(13)
     public void answerThirteen() {
         Assertions.assertEquals("Baby", this.exercise.thirteen(0));
         Assertions.assertEquals("Toddler", this.exercise.thirteen(1));
